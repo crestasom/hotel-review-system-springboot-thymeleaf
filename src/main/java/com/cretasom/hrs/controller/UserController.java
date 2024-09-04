650181dc8fef2d9749c6f5e7a724113f8489d9d8
crestasom <41513543+shrest0000@users.noreply.github.com>
2024-09-04 08:13:39 +0545

    added update page and update delete logic

--- src/main/java/com/cretasom/hrs/controller/UserController.java
+++ src/main/java/com/cretasom/hrs/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,7 +20,7 @@ public class UserController {
 
 	int id = 1;
 
-	@GetMapping("/")
+	@GetMapping({ "/", "/index" })
 	public String index(Model model) {
 		for (User user : userList) {
 
@@ -39,6 +40,28 @@ public class UserController {
 		id++;
 		userList.add(user);
 		// System.out.println(user.getName() + " " + user.getEmail());
+		return "redirect:/user/";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String showAddForm(@PathVariable int id, Model model) {
+		User user = userList.get(id - 1);
+		model.addAttribute("user", user);
+		return "user/update";
+	}
+
+	@PostMapping("/updateuser")
+	public String updateUser(User user) {
+		User user1 = userList.get(user.getId() - 1);
+		user1.setName(user.getName());
+		// System.out.println(user.getName() + " " + user.getEmail());
+		return "redirect:/user/";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteUser(@PathVariable int id, Model model) {
+		userList.remove(id - 1);
+		model.addAttribute("users", userList);
 		return "redirect:/user/";
 	}
 }
